0d3190a2db18104a7375b80762efbb9480cd830d
caozuohua <535863761@qq.com>
2017-01-10 15:57:21 +0800

    重新修改，加入两项配置检查

--- cephdeploy.sh
+++ cephdeploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#version: v4.2
+#version: v4.4
 #time: 2016.12.6
 #author: me
-#update@ 2016.12.27
+#update@ 2017.1.10
 #v1.0：创建——添加所有命令
 #version: v4.1
 #time: 2016.12.6
@@ -13,6 +13,8 @@
 #v4.0：单脚本部署
 #v4.1：配置文件内容重新定义
 #v4.2：增加设置chooseleaf = [0,1]（支持单节点部署）；filestore xattr use omap = false（云一般是不使用rgw和mds）
+#v4.3: [2017.1.10]修改mon节点数组，实际情况下一般mon节点数和总节点数不一样，所以另外加了个mon_node数组
+#v4.4: [2017.1.10]添加判断，如果配置的mon节点数和其ip数量不对应则报错退出；判断osd数量，最少应为2，否则报错退出
 
 ##########################################################################
 #路径常量，一般不修改
@@ -23,11 +25,26 @@ mon_init_node='node1'
 mon_add_node=('node2' 'node3')
 mon_node_ip="192.168.105.197,192.168.105.198,192.168.105.199"
 mon_add_length=${#mon_add_node[@]}
-
+mon_node=(${mon_init_node} ${mon_add_node[@]})
+mon_num=${#mon_node[@]}
 #osd信息初始化,根据实际情况修改
 osd_node=('node1' 'node2' 'node3')
 dev_list=('/dev/vda')
-all_node=${osd_node[@]}
+all_node=${osd_node[@]} 
+let osd_num=${#osd_node[@]}*${#dev_list[@]}
+
+let len=${mon_num}-1
+if [ -z `echo ${ipp}|cut -d \, -f ${len}` ]
+then 
+ echo 每个mon节点都应该对应一个IP！请重新配置脚本！
+ exit 1
+fi
+
+if [ '1' -ge ${#osd_num[@]} ]
+then
+ echo osd数量至少应为2（对应至少两块硬盘）！！
+ exit 1
+fi
 
 #请填写ceph.conf中要指定的配置项，根据实际情况修改
 #一般默认3副本
@@ -44,8 +61,6 @@ osd_pool_default_size='2'
 osd_pool_default_min_size='1'
 public_network='192.168.105.0/24'
 cluster_network='192.168.105.0/24'
-#osd_crush_chooseleaf_type=
-#filestore_xattr_use_omap=
 ##########################################################################
 
 
@@ -71,10 +86,10 @@ elif [ ${#all_node[@]} -eq '1' ]
 else
  osd_crush_chooseleaf_type='1'
 fi
-#---以下为配置文件必需的配置项---------------------------
+#------------------------以下为配置文件必需的配置项---------------------------
 echo "[global]" > ${cephconf}
 echo "fsid = ${ceph_uuid}" >> ${cephconf}
-echo "mon initial members = ${all_node[*]}" >> ${cephconf}
+echo "mon initial members = ${mon_node[*]}" >> ${cephconf}
 echo "mon host = ${mon_node_ip}" >> ${cephconf}
 echo "public network = ${public_network}" >> ${cephconf}
 echo "cluster network = ${cluster_network}" >> ${cephconf}
@@ -86,34 +101,10 @@ echo "osd pool default size = ${osd_pool_default_size}" >> ${cephconf}
 echo "osd pool default min size = ${osd_pool_default_min_size}" >> ${cephconf}
 echo "filestore xattr use omap = false" >> ${cephconf}
 echo "osd crush chooseleaf type = ${osd_crush_chooseleaf_type}" >> ${cephconf}
-#echo "osd pool default pg num = ${osd_pool_default_pg_num}" >> ${cephconf}
-#echo "osd pool default pgp num = ${osd_pool_default_pgp_num}" >> ${cephconf}
-#echo "osd crush update on start  = ${osd_crush_update_on_start}" >> ${cephconf}
-#echo "[mon.a]" >> ${cephconf}
-#echo "mon_host = ${mon_init_node}" >> ${cephconf}
-#echo "mon_addr = ${mon_init_node_ip_with_port}" >> ${cephconf}
+echo "osd pool default pg num = ${osd_pool_default_pg_num}" >> ${cephconf}
+echo "osd pool default pgp num = ${osd_pool_default_pgp_num}" >> ${cephconf}
+echo "osd crush update on start  = ${osd_crush_update_on_start}" >> ${cephconf}
 #---------------------------------------------------------
-#if [ ${mon_add_length} -gt '0' ]
-#then
-# for (( j=0;j<${mon_add_length};j++ ))
-#  do
-#   let mj=${j}+1
-#   echo "[mon.${mon_id[${mj}]}]" >> ${cephconf}
-#   echo "  mon_host = ${mon_add_node[${j}]}" >> ${cephconf}
-#   echo "  mon_addr = ${mon_add_node_ip_with_port[${j}]}" >> ${cephconf}
-#  done
-#fi
-#osd_id='0'
-#for i in ${osd_node[@]}
-# do
-#  for dev in ${dev_list[@]}
-#   do
-#       echo "[osd.${osd_id}]" >> ${cephconf}
-#       echo "  host = ${i}" >> ${cephconf}
-#       echo "  devs = ${dev}" >> ${cephconf}
-#       let osd_id=${osd_id}+1
-#   done
-#done
 
 echo '++++++++++++++++++++++++++ceph集群部署开始+++++++++++++++++++++++++++++++'
 echo '开始scp配置文件...'
